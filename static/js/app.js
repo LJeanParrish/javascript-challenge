c99d829ed6ec3f4cc2ee864329c5d55236dc2025
Lauren Parrish <ljeanparrish@gmail.com>
2021-03-30 16:43:23 -0400

    updates to html

--- static/js/app.js
+++ static/js/app.js
@@ -40,7 +40,7 @@ data.forEach(function(ufoReport) {
   });
 });
 
-// // Step 5: Use d3 to update each cell's text with ufor report values
+// Step 5: Use d3 to update each cell's text with ufor report values
 data.forEach(function(ufoReport) {
   console.log(ufoReport);
   var row = tbody.append("tr");
@@ -61,18 +61,18 @@ form.on("submit",runEnter);
 // Create the function to run the event
 function runEnter() {
 
- // Prevent the page from refreshing
+// Prevent the page from refreshing
  d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
+// Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
+// Get the value property of the input element
   var inputValue = inputElement.property("value");
 
-  // Print the value to the console
+// Print the value to the console
   console.log(inputValue);
 
-  // Set the span tag in the h1 element to the text that was entered in the form
+// Set the span tag in the h1 element to the text that was entered in the form
   d3.select("h1>span").text(inputValue);
 }
