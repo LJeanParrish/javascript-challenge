98224042620c37a614d7aa0505f77e3681e69358
Lauren Parrish <ljeanparrish@gmail.com>
2021-03-30 16:21:36 -0400

    initial form code

--- static/js/app.js
+++ static/js/app.js
@@ -51,3 +51,28 @@ data.forEach(function(ufoReport) {
     cell.text(value);
   });
 });
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers for pressing the enter key
+form.on("submit",runEnter);
+
+// Create the function to run the event
+function runEnter() {
+
+ // Prevent the page from refreshing
+ d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Print the value to the console
+  console.log(inputValue);
+
+  // Set the span tag in the h1 element to the text that was entered in the form
+  d3.select("h1>span").text(inputValue);
+}
