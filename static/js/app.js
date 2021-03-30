b4e2b9cd583d267426278ce5c834a3dcbfb45a25
Lauren Parrish <ljeanparrish@gmail.com>
2021-03-30 17:20:17 -0400

    removal of code duplications to streamline

--- static/js/app.js
+++ static/js/app.js
@@ -1,78 +1,53 @@
+////////////////////////////////////////////////////////////////////////
+//PART 1 - AUTOMATIC TABLE
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the ufo data from data.js
-console.log(data);
-
 // Step 1: Loop Through `data` and console.log each UFO report object
-data.forEach(function(ufoReport) {
+tableData.forEach(function(ufoReport) {
     console.log(ufoReport);
-});
+    
+    // Step 2:  Use d3 to append one table row `tr` for each ufo report object
+    var row = tbody.append("tr");
+    
+    // Step 3:  Use `Object.entries` to console.log each ufo report value
+    Object.entries(ufoReport).forEach(function([key, value]) {
+    console.log(key, value);  
 
-// Step 2:  Use d3 to append one table row `tr` for each ufo report object
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-  var row = tbody.append("tr");
-});
-
-// Step 3:  Use `Object.entries` to console.log each ufo report value
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-  var row = tbody.append("tr");
-
-  Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);
-  });
-});
-
-// Step 4: Use d3 to append 1 cell per ufo report value
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-  var row = tbody.append("tr");
-
-  Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);
-    // Append a cell to the row for each value in the ufo report object
-    var cell = row.append("td");
-  });
-});
-
-// Step 5: Use d3 to update each cell's text with ufor report values
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-  var row = tbody.append("tr");
-  Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);
     // Append a cell to the row for each value in the ufo report object
     var cell = row.append("td");
     cell.text(value);
   });
 });
 
-// Select the form
-var form = d3.select("#form");
 
-// Create event handlers for pressing the enter key
-form.on("submit",runEnter);
+///////////////////////////////////////////////////////////////////////////////////////////  
+//PART 2 - DATA SEARCH
+// // Select the form
+// var form = d3.select("#form");
 
-// Create the function to run the event
-function runEnter() {
+// // Create event handlers for pressing the enter key
+// form.on("submit",runEnter);
 
-// Prevent the page from refreshing
- d3.event.preventDefault();
+// // Create the function to run the event
+// function runEnter() {
 
-// Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+// // Prevent the page from refreshing
+//  d3.event.preventDefault();
 
-// Get the value property of the input element
-  var inputValue = inputElement.property("value");
+// // Select the input element and get the raw HTML node
+//   var inputElement = d3.select("#datetime");
 
-// Print the value to the console
-  console.log(inputValue);
+// // Get the value property of the input element
+//   var inputValue = inputElement.property("value");
 
-// Set the span tag in the h1 element to the text that was entered in the form
-  d3.select("h1>span").text(inputValue);
-}
+// // Print the value to the console
+//   console.log(inputValue);
+
+// // Set the span tag in the h1 element to the text that was entered in the form
+//   d3.select("h1>span").text(inputValue);
+// }
