1a81267e0746f11d4354527d8000cd30e0708df2
Lauren Parrish <ljeanparrish@gmail.com>
2021-04-01 08:44:27 -0400

    updated code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,15 +8,15 @@ console.log(tableData);
 var tbody = d3.select("tbody");
 
 // Step 1: Loop Through `data` and console.log each UFO report object
-tableData.forEach(function(ufoReport) {
-    console.log(ufoReport);
-    
-    // Step 2:  Use d3 to append one table row `tr` for each ufo report object
-    var row = tbody.append("tr");
-    
-    // Step 3:  Use `Object.entries` to console.log each ufo report value
-    Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);  
+tableData.forEach(function (ufoReport) {
+  console.log(ufoReport);
+
+  // Step 2:  Use d3 to append one table row `tr` for each ufo report object
+  var row = tbody.append("tr");
+
+  // Step 3:  Use `Object.entries` to console.log each ufo report value
+  Object.entries(ufoReport).forEach(function ([key, value]) {
+    console.log(key, value);
 
     // Append a cell to the row for each value in the ufo report object
     var cell = row.append("td");
@@ -28,32 +28,37 @@ tableData.forEach(function(ufoReport) {
 //PART 2 - DATA SEARCH
 
 // Assign the data from `data.js` to a descriptive variable
-var ufo = data;
+var ufos = data;
 
 // Select the button
 var button = d3.select("#button");
 
-// // Select the form
+// Select the form
 var form = d3.select("#form");
 
-// // Create event handlers for pressing the enter key
+// Create event handlers for pressing the enter key
 button.on("click", runEnter);
-form.on("submit",runEnter);
+form.on("submit", runEnter);
 
-// // Create the function to run the event
+// Create the function to run the event
 function runEnter() {
 
-// // Prevent the page from refreshing
- d3.event.preventDefault();
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 
-// // Select the input element and get the raw HTML node
+  // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
-// // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
 
-// // Print the value to the console
-//   console.log(inputValue);
+  // Print the value to the console
+  console.log(inputValue);
+  console.log(ufos);
+
+  var filteredData = ufos.filter(ufo => ufo.datetime === inputValue);
+
+  console.log(filteredData);
 
 // // Set the span tag in the h1 element to the text that was entered in the form
 //   d3.select("h1>span").text(inputValue);
